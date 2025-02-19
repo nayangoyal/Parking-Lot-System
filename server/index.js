2f98aea60f46e1ffa44d0d045badf5aa9a29f338
Nayan Goyal <nayangoyal94134@gmail.com>
2025-02-19 19:18:16 +0530

    CORS ERROR

--- server/index.js
+++ server/index.js
@@ -4,10 +4,32 @@ const bodyParser = express.json;
 const cors = require("cors");
 const { PORT } = process.env;
 
-
 const app = express();
+const whitelist = [
+  "http://localhost:5173",
+  "https://parking-system.vercel.app/",
+  "https://localhost", //This origin for mobile
+]; // add your origins here
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("Printing ORIGIN");
+    console.log(origin);
+    // used !origin because /admin/approve or reject me sending link via gmail..and on clicking that we dont have a origin
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Allowed ORIGIN request");
+      callback(null, true);
+    } else {
+      console.log("Blacklisted ORIGIN request");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use(cors());
+
+
+// app.use(cors());
 app.use(bodyParser());
 
 app.use('/parking', require('./routes/parkingRoutes'));
